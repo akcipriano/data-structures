9f2b75c211ddbdcf351ed29d347949c5895c0728
Alyssa Cipriano <alyssacipriano@gmail.com>
2018-11-28 21:13:10 -0800

    complete pseudoclassical stack

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
   }
 
+  size() {
+    return Object.keys(this.storage).length;
+  }
+
+  push(value) {
+    numKey++;
+    this.storage[numKey] = value;
+  }
+
+  pop() {
+    var lastItem = this.storage[numKey];
+    delete this.storage[numKey];
+    numKey--;
+    return lastItem;
+  }
 }
+
+var numKey = 0;
