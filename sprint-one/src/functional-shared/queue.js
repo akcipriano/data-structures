261e1746f70e746263dcfec75469802594245559
Alyssa Cipriano <alyssacipriano@gmail.com>
2018-11-28 19:13:44 -0800

    add code for size method

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,22 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var holder = {};
+
+  holder.storage = {};
+
+  _.extend(holder, queueMethods);
+
+  return holder;
+
 };
 
 var queueMethods = {};
+var numKey = 0;
+
+queueMethods.size = function() {
+  var storage = this.storage;
+  return Object.keys(storage).length;
+};
 
 
