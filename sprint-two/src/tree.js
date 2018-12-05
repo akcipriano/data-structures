50e9982566218da4dc8fa455a9b267df72a96998
Andrew Myint <doku.potharhtoo@gmail.com>
2018-12-05 00:46:46 -0500

    unfinished tree on last task.

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,40 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = [];  // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  this.children.push(Tree(value));
 };
 
 treeMethods.contains = function(target) {
+  // var holder = false;
+  // if (this.children.length) {
+  //   for (var i = 0; i < this.children.length; i++) {
+  //     if (_.pluck(this.children[i], 'value').includes(target)) {
+  //       holder = true;
+  //     }
+
+  //     // this.children[i].contains(target);
+  //   }
+  // }
+  // if (_.pluck(this.children, 'value').includes(target)){
+  //   return _.pluck(this.children, 'value').includes(target);
+  // }
+
+  if (this.children.length > 1) {
+    for (var i = 0; i < this.children.length; i++) {
+      var temp =  this.children[i].contains(target);
+    }
+    return temp;
+  }
+  return _.pluck(this.children, 'value').includes(target);
+
 };
 
 
